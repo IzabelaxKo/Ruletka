87da08e62cd60d4bb029d5d392a2f6df888abca6
Szymon Zielinski <zielinskiszymon89@gmail.com>
2025-03-04 19:45:44 +0100

    Button

---
+++ Ruletka/Main.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ruletka
+{
+    public partial class Main : Form
+    {
+
+
+        public Main()
+        {
+            InitializeComponent();
+
+            RoundedButton registerButton = new RoundedButton
+            {
+                Text = "Kliknij mnie",
+                Size = new Size(150, 50),
+                Location = new Point(380, 400),
+                BackColor = Color.CornflowerBlue,
+                ForeColor = Color.White,
+            };
+            registerButton.Click += (sender, e) => Rejestrowanie(); // Dodanie zdarzenia kliknięcia
+            this.Controls.Add(registerButton);
+
+            RoundedButton loginButton = new RoundedButton
+            {
+                Text = "Zaloguj",
+                Size = new Size(150, 50),
+                Location = new Point(380, 300),
+                BackColor = Color.CornflowerBlue,
+                ForeColor = Color.White,
+                BorderRadius = 10,
+            };
+            loginButton.Click += (sender, e) => Logowanie(); // Dodanie zdarzenia kliknięcia
+            this.Controls.Add(loginButton);
+        }
+
+        private void Logowanie()
+        {
+            Login loginPage = new Login();
+            this.Hide();
+            loginPage.ShowDialog();
+            this.Close();
+        }
+        private void Rejestrowanie()
+        {
+            Register register = new Register();
+            this.Hide();
+            register.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ Ruletka/RoundedButton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+public class RoundedButton : Button
+{
+    public int BorderRadius { get; set; } = 20; // Promień zaokrąglenia
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+
+        GraphicsPath path = new GraphicsPath();
+        path.AddArc(0, 0, BorderRadius * 2, BorderRadius * 2, 180, 90);
+        path.AddArc(Width - BorderRadius * 2, 0, BorderRadius * 2, BorderRadius * 2, 270, 90);
+        path.AddArc(Width - BorderRadius * 2, Height - BorderRadius * 2, BorderRadius * 2, BorderRadius * 2, 0, 90);
+        path.AddArc(0, Height - BorderRadius * 2, BorderRadius * 2, BorderRadius * 2, 90, 90);
+        path.CloseFigure();
+
+        this.Region = new Region(path);
+        e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+        using (SolidBrush brush = new SolidBrush(this.BackColor))
+        {
+            e.Graphics.FillPath(brush, path);
+        }
+
+        using (Pen pen = new Pen(this.ForeColor, 2))
+        {
+            e.Graphics.DrawPath(pen, path);
+        }
+
+        StringFormat sf = new StringFormat
+        {
+            Alignment = StringAlignment.Center,
+            LineAlignment = StringAlignment.Center
+        };
+
+        e.Graphics.DrawString(this.Text, this.Font, new SolidBrush(this.ForeColor), this.ClientRectangle, sf);
+    }
+}
